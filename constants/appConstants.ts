29f2fce50b2e923a468d849400e75960fdcfca26
semoozaher-wq <semoo.zaher@gmail.com>
2026-09-23 07:49:25 +0300

    Add appConstants with pain-related constants

---
+++ constants/appConstants.ts
@@ -0,0 +1,16 @@
+// constants/appConstants.ts
+
+export const DATA = {
+  HISTORY_STORAGE_KEY: 'bodymap-pain-history-v2',
+};
+
+export const PAIN_TYPES = ['حرقان', 'طعن', 'ضغط', 'مستمر', 'متقطع'];
+
+export const DURATIONS = ['منذ ساعات', 'منذ أيام', 'منذ أسابيع'];
+
+export const RED_FLAGS = [
+  'ضيق نفس أو ألم ضاغط بالصدر',
+  'إغماء أو ارتباك شديد',
+  'ضعف مفاجئ أو صعوبة في الكلام',
+  'إصابة قوية أو نزيف',
+];
